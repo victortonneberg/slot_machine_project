04e30eeccc92df796518ec662197f03f585ff70f
victortonneberg <84105735+victortoneberg@users.noreply.github.com>
2024-09-20 14:22:37 +0200

    First push

---
+++ project.js
@@ -0,0 +1,15 @@
+// 1. Deposit some money
+// 2. Detemine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ")
+}
+
+deposit()
